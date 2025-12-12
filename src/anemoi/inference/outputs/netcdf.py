a9d2da2984722017bc554a4d06ccaa3c85213892
chjulie <julie.charlet@epfl.ch>
2025-12-11 16:39:35 -0800

    fix: specify gregorian calendar for initial_date_var

--- src/anemoi/inference/outputs/netcdf.py
+++ src/anemoi/inference/outputs/netcdf.py
@@ -152,7 +152,7 @@ class NetCDFOutput(Output):
             self.initial_date_var = self.ncfile.createVariable("initial_date", "f8", ("initial_date",), **compression)
             self.initial_date_var.units = "seconds since 1970-01-01 00:00:00"
             self.initial_date_var.long_name = "initial_date"
-            self.initial_date_var.calendar = "proleptic_gregorian"
+            self.initial_date_var.calendar = "gregorian"
 
         # Pre-fill the lead_time values (in hours)
         # lead_times = [(i * time_step).total_seconds() / 3600 for i in range(time)]
